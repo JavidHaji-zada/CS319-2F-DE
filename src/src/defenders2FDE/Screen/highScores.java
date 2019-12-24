3543e0f4aa4bc72b7fa4d2fd57ed992888fe8096
busraUnver <busra.unver@ug.bilkent.edu.tr>
2019-12-24 05:59:11 +0300

    High Scores screens

--- src/src/defenders2FDE/Screen/highScores.java
+++ src/src/defenders2FDE/Screen/highScores.java
@@ -1,4 +1,19 @@
 package defenders2FDE.Screen;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
 public class highScores {
+    @FXML
+    Button backButton;
+    public void backToMain(javafx.event.ActionEvent actionEvent) throws Exception{
+        Stage window;
+        window = (Stage) backButton.getScene().getWindow();
+        Pane root = FXMLLoader.load(getClass().getResource("fxml/MainScreen.fxml"));
+        window.setScene(new Scene(root));
+    }
 }
